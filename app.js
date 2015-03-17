4399e7f4b6c0a0d16e03c06abb9f99279f7df51c
Arpit Agrawal <arpit.agrawal.27@gmail.com>
2015-03-17 19:10:52 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,104 @@
+var fs = require('fs');
+var regexp=require('node-regexp');
+var re = regexp().end('.mp4').toRegExp(),re1=regexp().end('.flv').toRegExp(),re2=regexp().end('.mkv').toRegExp(),re3=regexp().end('.ogg').toRegExp(),re5=regexp().end('.dctmp').toRegExp();
+var express=require('express');
+var bodyParser     =         require("body-parser");
+var app=express();
+var map={},map1={},map3={};
+var http = require('http'),fs = require('fs'),util = require('util');
+var util  = require('util'),
+    spawn = require('child_process').spawn;
+    var co=0;
+    var has_conn=0;
+    var search_req=0;
+    var map={};
+    var cur_mov_str;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+
+	res.send('ok');
+
+});
+
+app.get('/downloads',function(req,res){
+	var response='';
+
+	var data=fs.readFileSync('shared','utf8');
+  	//console.log(data);	
+
+	  		data=data.split(';');
+	  		
+	  		for(var i=0;i<data.length;i++)
+	  		{
+	  			console.log(data[i]);
+	  			var files=fs.readdirSync(data[i]);
+	  				 
+	  				 var id=0;
+	  				 var directory=data[i];
+	  				 for(var i in files)
+	  				 {
+	     					 
+	     					 var file=files[i];
+	     					 //console.log(file);
+	     					 if(re.test(file))
+	     					 {
+	     					 	//console.log(file);
+	      						response +=  file +'\n' ;
+	   	
+	      						map[id]=directory;
+	      						map1[id]= file;	
+	      						id+=1
+	 					 }
+	   				 
+	   				 }
+	  		
+	  		}
+			if(id==0){
+				console.log("Please share some files\n");
+				process.exit(code=0);
+			}
+			console.log(response);
+			res.send(response);
+});
+
+app.get('/downloads/*', function (req, res) {
+  var id='';
+  for(var i=11;i<req.url.length;i++)id+=req.url[i];
+    console.log(id);
+  var path = map[id] + '/' + map1[id];
+  console.log(path);
+  var stat = fs.statSync(path);
+  var total = stat.size;
+  if (req.headers['range']) {
+    var range = req.headers.range;
+    var parts = range.replace(/bytes=/, "").split("-");
+    var partialstart = parts[0];
+    var partialend = parts[1];
+ 
+    var start = parseInt(partialstart, 10);
+    var end = partialend ? parseInt(partialend, 10) : total-1;
+    var chunksize = (end-start)+1;
+    console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
+ 
+    var file = fs.createReadStream(path, {start: start, end: end});
+    res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+    file.pipe(res);
+  } else {
+    console.log('ALL: ' + total);
+    res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+    fs.createReadStream(path).pipe(res);
+  }
+});
+
+app.get('/readjquery',function(req,res){
+	fs.readFile('jquery.min.js', 'utf8', function (err,data) {
+  		if (err) {
+    			 console.log(err);
+  		}
+  		res.send(data);
+	});
+});
+
+app.listen(3000);
